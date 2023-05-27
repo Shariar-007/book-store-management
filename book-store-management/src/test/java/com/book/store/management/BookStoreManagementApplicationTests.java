0ac9b1a89bd7fb6c7c72798859b8babef9bc2ab9
mohammad alshariar <mohammadalshariar@mohammads-MacBook-Pro.local>
2023-05-27 16:56:15 +0600

    FIRST COMMIT

---
+++ book-store-management/src/test/java/com/book/store/management/BookStoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.store.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
